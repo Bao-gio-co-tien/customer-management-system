eef2068718f6010a913c0a52b60c73f4a1680f23
Manh1504 <evil1504@gmail.com>
2025-02-19 12:52:59 +0700

    Delete src/main/java/soa/group/project/cms/impl/email/src directory

--- src/main/java/soa/group/project/cms/impl/email/src/main/java/com/laptrinhb2a/springboot/mailsender/service/MailService.java
+++
@@ -1,9 +0,0 @@
-package com.laptrinhb2a.springboot.mailsender.service;
-
-import com.laptrinhb2a.springboot.mailsender.dto.DataMailDTO;
-
-import javax.mail.MessagingException;
-
-public interface MailService {
-    void sendHtmlMail(DataMailDTO dataMail, String templateName) throws MessagingException;
-}

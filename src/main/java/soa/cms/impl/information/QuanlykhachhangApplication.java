54e7487f9445ecf3b7997dc7babc059f87e61d26
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2025-02-21 01:19:03 +0700

    update 21/2

---
+++ src/main/java/soa/cms/impl/information/QuanlykhachhangApplication.java
@@ -0,0 +1,14 @@
+package soa.cms.impl.information;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableMongoRepositories("com.example.quanlykhachhang.repository")
+public class QuanlykhachhangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlykhachhangApplication.class, args);
+	}
+
+}
---
+++ src/main/java/soa/cms/impl/information/service/CustomerService.java
@@ -0,0 +1,40 @@
+package soa.cms.impl.information.service;
+
+import com.example.customermanagement.model.Customer;
+import com.example.customermanagement.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(String id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer addCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(String id, Customer updatedCustomer) {
+        return customerRepository.findById(id).map(customer -> {
+            customer.setName(updatedCustomer.getName());
+            customer.setEmail(updatedCustomer.getEmail());
+            customer.setPhone(updatedCustomer.getPhone());
+            return customerRepository.save(customer);
+        }).orElse(null);
+    }
+
+    public void deleteCustomer(String id) {
+        customerRepository.deleteById(id);
+    }
+}

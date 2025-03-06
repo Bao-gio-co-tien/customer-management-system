404b4a16ace5d10f5a8367f558105bf073c2fde7
Bao-gio-co-tien <129248299+Bao-gio-co-tien@users.noreply.github.com>
2025-03-07 04:36:21 +0700

    update

--- src/test/java/soa/cms/CustomerManagementSystemApplicationTests.java
+++ src/test/java/soa/cms/CustomerManagementSystemApplicationTests.java
@@ -2,10 +2,18 @@ package soa.cms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ComponentScan(
+	excludeFilters = @ComponentScan.Filter(
+			type = FilterType.ASSIGNABLE_TYPE,
+			classes = soa.cms.corba.Config.class
+	)
+)
 class CustomerManagementSystemApplicationTests {
 
 	@Test
--- src/test/java/soa/cms/corba/TestCorbaConfig.java
+++ src/test/java/soa/cms/corba/TestCorbaConfig.java
@@ -6,7 +6,6 @@ import org.omg.CosNaming.NamingContextExt;
 import org.omg.PortableServer.POA;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
@@ -14,19 +13,16 @@ import org.springframework.context.annotation.Profile;
 public class TestCorbaConfig {
 
     @Bean
-    @Primary
     public ORB orb() {
         return Mockito.mock(ORB.class);
     }
 
     @Bean
-    @Primary
     public POA rootPOA() {
         return Mockito.mock(POA.class);
     }
 
     @Bean
-    @Primary
     public NamingContextExt namingContextExt() {
         return Mockito.mock(NamingContextExt.class);
     }
